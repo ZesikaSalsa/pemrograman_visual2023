8c370b11718b4e78d991e43a76050287a8897254
Zesika Salsa Zahara <103353942+ZesikaSalsa@users.noreply.github.com>
2023-04-13 22:20:43 +0700

    Add files via upload

---
+++ latihan5/latihan5/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNilaiAkhir = New System.Windows.Forms.TextBox()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.btnSubmit.Location = New System.Drawing.Point(263, 71)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(106, 46)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(32, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nilai Akhir"
+        '
+        'txtNilaiAkhir
+        '
+        Me.txtNilaiAkhir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtNilaiAkhir.Location = New System.Drawing.Point(37, 76)
+        Me.txtNilaiAkhir.Name = "txtNilaiAkhir"
+        Me.txtNilaiAkhir.Size = New System.Drawing.Size(208, 35)
+        Me.txtNilaiAkhir.TabIndex = 2
+        Me.txtNilaiAkhir.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Enabled = False
+        Me.txtKeterangan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.txtKeterangan.Location = New System.Drawing.Point(37, 182)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(430, 30)
+        Me.txtKeterangan.TabIndex = 4
+        Me.txtKeterangan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label2.Location = New System.Drawing.Point(32, 143)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Keterangan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Papyrus", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(318, 258)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(169, 38)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Zesika Salsa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Pink
+        Me.ClientSize = New System.Drawing.Size(499, 305)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNilaiAkhir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form1"
+        Me.Text = "Kelulusan"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNilaiAkhir As TextBox
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ latihan5/latihan5/Form2.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form2
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If (Val(txtMasaKerja.Text) >= 20) Then
+            txtKeterangan.Text = "Anda mendapat bonus emas 5gr"
+
+        ElseIf (txtMasaKerja.Text >= 15) Then
+            txtKeterangan.Text = "Anda mendapat bonus tiket liburan"
+
+        ElseIf (txtMasaKerja.Text >= 10) Then
+            txtKeterangan.Text = "Anda mendapat bonus kuota internet 50gb"
+
+        ElseIf (txtMasaKerja.Text >= 5) Then
+            txtKeterangan.Text = "Anda mendapat bonus kuota internet 20gb"
+
+        Else
+            txtKeterangan.Text = "Maaf anda belum layak dapat bonus"
+        End If
+    End Sub
+End Class
