ac8dec8234646ad64d659fba4521f1edbaec192c
Zesika Salsa Zahara <103353942+ZesikaSalsa@users.noreply.github.com>
2023-05-03 19:07:19 +0700

    Add files via upload

---
+++ tugas4/ConvertKelvin/ConvertKelvin/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtKelvin = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtReamur = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtKelvin
+        '
+        Me.txtKelvin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKelvin.Location = New System.Drawing.Point(215, 66)
+        Me.txtKelvin.Name = "txtKelvin"
+        Me.txtKelvin.Size = New System.Drawing.Size(124, 35)
+        Me.txtKelvin.TabIndex = 26
+        Me.txtKelvin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(46, 72)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 28)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Kelvin"
+        '
+        'txtReamur
+        '
+        Me.txtReamur.Enabled = False
+        Me.txtReamur.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtReamur.Location = New System.Drawing.Point(215, 358)
+        Me.txtReamur.Name = "txtReamur"
+        Me.txtReamur.Size = New System.Drawing.Size(124, 35)
+        Me.txtReamur.TabIndex = 24
+        Me.txtReamur.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(46, 364)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 28)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "Reamur"
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Enabled = False
+        Me.txtFahrenheit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFahrenheit.Location = New System.Drawing.Point(215, 292)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(124, 35)
+        Me.txtFahrenheit.TabIndex = 22
+        Me.txtFahrenheit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(46, 298)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 28)
+        Me.Label2.TabIndex = 21
+        Me.Label2.Text = "Fahrenheit"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Hobo Std", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.ForeColor = System.Drawing.Color.Black
+        Me.btnConvert.Location = New System.Drawing.Point(215, 140)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(124, 46)
+        Me.btnConvert.TabIndex = 20
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Enabled = False
+        Me.txtCelcius.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCelcius.Location = New System.Drawing.Point(215, 227)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(124, 35)
+        Me.txtCelcius.TabIndex = 19
+        Me.txtCelcius.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(46, 233)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 28)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Celcius"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(396, 461)
+        Me.Controls.Add(Me.txtKelvin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtReamur)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "Konversi Kelvin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtKelvin As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtReamur As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ tugas4/ConvertReamur/ConvertReamur/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtKelvin = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtReamur = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtKelvin
+        '
+        Me.txtKelvin.Enabled = False
+        Me.txtKelvin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKelvin.Location = New System.Drawing.Point(216, 359)
+        Me.txtKelvin.Name = "txtKelvin"
+        Me.txtKelvin.Size = New System.Drawing.Size(124, 35)
+        Me.txtKelvin.TabIndex = 35
+        Me.txtKelvin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(47, 365)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 28)
+        Me.Label4.TabIndex = 34
+        Me.Label4.Text = "Kelvin"
+        '
+        'txtReamur
+        '
+        Me.txtReamur.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtReamur.Location = New System.Drawing.Point(216, 67)
+        Me.txtReamur.Name = "txtReamur"
+        Me.txtReamur.Size = New System.Drawing.Size(124, 35)
+        Me.txtReamur.TabIndex = 33
+        Me.txtReamur.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(47, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 28)
+        Me.Label3.TabIndex = 32
+        Me.Label3.Text = "Reamur"
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Enabled = False
+        Me.txtFahrenheit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFahrenheit.Location = New System.Drawing.Point(216, 293)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(124, 35)
+        Me.txtFahrenheit.TabIndex = 31
+        Me.txtFahrenheit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(47, 299)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 28)
+        Me.Label2.TabIndex = 30
+        Me.Label2.Text = "Fahrenheit"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Hobo Std", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.ForeColor = System.Drawing.Color.Black
+        Me.btnConvert.Location = New System.Drawing.Point(216, 141)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(124, 46)
+        Me.btnConvert.TabIndex = 29
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Enabled = False
+        Me.txtCelcius.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCelcius.Location = New System.Drawing.Point(216, 228)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(124, 35)
+        Me.txtCelcius.TabIndex = 28
+        Me.txtCelcius.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Perpetua", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(47, 234)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 28)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Celcius"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(387, 461)
+        Me.Controls.Add(Me.txtKelvin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtReamur)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Konversi Reamur"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtKelvin As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtReamur As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents Label1 As Label
+End Class
