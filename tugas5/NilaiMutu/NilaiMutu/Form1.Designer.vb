064f2bb03b49b8a4d4e81d8f12e8b07242c70968
Zesika Salsa Zahara <103353942+ZesikaSalsa@users.noreply.github.com>
2023-05-16 20:05:00 +0700

    Add files via upload

---
+++ tugas5/NilaiMutu/NilaiMutu/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNilaiMutu = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtNilaiAkhir = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtUas = New System.Windows.Forms.TextBox()
+        Me.txtUts = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtTgs = New System.Windows.Forms.TextBox()
+        Me.txtKehadiran = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNim = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNilaiMutu
+        '
+        Me.txtNilaiMutu.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtNilaiMutu.Location = New System.Drawing.Point(423, 513)
+        Me.txtNilaiMutu.Name = "txtNilaiMutu"
+        Me.txtNilaiMutu.Size = New System.Drawing.Size(90, 35)
+        Me.txtNilaiMutu.TabIndex = 59
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(299, 518)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(111, 27)
+        Me.Label9.TabIndex = 58
+        Me.Label9.Text = "Nilai Mutu"
+        '
+        'txtNilaiAkhir
+        '
+        Me.txtNilaiAkhir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtNilaiAkhir.Location = New System.Drawing.Point(179, 513)
+        Me.txtNilaiAkhir.Name = "txtNilaiAkhir"
+        Me.txtNilaiAkhir.Size = New System.Drawing.Size(90, 35)
+        Me.txtNilaiAkhir.TabIndex = 57
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(48, 518)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(110, 27)
+        Me.Label10.TabIndex = 56
+        Me.Label10.Text = "Nilai Akhir"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.Location = New System.Drawing.Point(179, 439)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(334, 43)
+        Me.btnHitung.TabIndex = 55
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'txtUas
+        '
+        Me.txtUas.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtUas.Location = New System.Drawing.Point(423, 368)
+        Me.txtUas.Name = "txtUas"
+        Me.txtUas.Size = New System.Drawing.Size(90, 35)
+        Me.txtUas.TabIndex = 54
+        '
+        'txtUts
+        '
+        Me.txtUts.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtUts.Location = New System.Drawing.Point(423, 318)
+        Me.txtUts.Name = "txtUts"
+        Me.txtUts.Size = New System.Drawing.Size(90, 35)
+        Me.txtUts.TabIndex = 53
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(339, 373)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 27)
+        Me.Label7.TabIndex = 52
+        Me.Label7.Text = "UAS"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(339, 323)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 27)
+        Me.Label8.TabIndex = 51
+        Me.Label8.Text = "UTS"
+        '
+        'txtTgs
+        '
+        Me.txtTgs.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtTgs.Location = New System.Drawing.Point(179, 368)
+        Me.txtTgs.Name = "txtTgs"
+        Me.txtTgs.Size = New System.Drawing.Size(90, 35)
+        Me.txtTgs.TabIndex = 50
+        '
+        'txtKehadiran
+        '
+        Me.txtKehadiran.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtKehadiran.Location = New System.Drawing.Point(179, 318)
+        Me.txtKehadiran.Name = "txtKehadiran"
+        Me.txtKehadiran.Size = New System.Drawing.Size(90, 35)
+        Me.txtKehadiran.TabIndex = 49
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(49, 373)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(68, 27)
+        Me.Label6.TabIndex = 48
+        Me.Label6.Text = "Tugas"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(48, 323)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(108, 27)
+        Me.Label5.TabIndex = 47
+        Me.Label5.Text = "Kehadiran"
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtProdi.Location = New System.Drawing.Point(179, 249)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(334, 35)
+        Me.txtProdi.TabIndex = 46
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtNama.Location = New System.Drawing.Point(179, 186)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(334, 35)
+        Me.txtNama.TabIndex = 45
+        '
+        'txtNim
+        '
+        Me.txtNim.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtNim.Location = New System.Drawing.Point(179, 126)
+        Me.txtNim.Name = "txtNim"
+        Me.txtNim.Size = New System.Drawing.Size(334, 35)
+        Me.txtNim.TabIndex = 44
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(48, 254)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 27)
+        Me.Label4.TabIndex = 43
+        Me.Label4.Text = "Prodi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(48, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 27)
+        Me.Label3.TabIndex = 42
+        Me.Label3.Text = "Nama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(48, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 27)
+        Me.Label2.TabIndex = 41
+        Me.Label2.Text = "NIM"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Minion Pro", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(173, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(237, 33)
+        Me.Label1.TabIndex = 40
+        Me.Label1.Text = "Nilai Mutu Mahasiswa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(560, 598)
+        Me.Controls.Add(Me.txtNilaiMutu)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtNilaiAkhir)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtUas)
+        Me.Controls.Add(Me.txtUts)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtTgs)
+        Me.Controls.Add(Me.txtKehadiran)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtNim)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNilaiMutu As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtNilaiAkhir As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtUas As TextBox
+    Friend WithEvents txtUts As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtTgs As TextBox
+    Friend WithEvents txtKehadiran As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNim As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
